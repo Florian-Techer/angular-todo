bb79bb53432c83ad484c487c6ba6b62a97f22529
Florian-Techer <techerflorian1@gmail.com>
2023-10-12 13:19:48 +0200

    add state and getter

--- src/app/todos/containers/todos/todos.component.ts
+++ src/app/todos/containers/todos/todos.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ITodo } from '../../interfaces/todos.interface';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit{
+  private todosState: ITodo[] = [];
+  constructor() { }
 
+  get todos(): ITodo[]{
+    return this.todosState.filter(todo => !todo.isCompleted)
+  }
+
+  get completedTodos(): ITodo[]{
+    return this.todosState.filter(todo => todo.isCompleted)
+  }
+
+  get countTodos(): number{
+    return this.todos.length
+  }
+
+  get countCompletedTodos(): number{
+    return this.completedTodos.length
+  }
+
+  get isHidden(): boolean{
+    return this.countCompletedTodos === 0
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
